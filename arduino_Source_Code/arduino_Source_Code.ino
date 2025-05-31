b80f19a7c18d05edf8a6f2ecc22f8ca3414a6b97
leeLaPPP <zokeoz12@nate.com>
2025-05-31 16:47:39 +0900

    아두이노 코드 수정

--- arduino_Source_Code/arduino_Source_Code.ino
+++ arduino_Source_Code/arduino_Source_Code.ino
@@ -5,6 +5,14 @@
 #define DHT11_TYPE DHT11
 #define MQ135_PIN A0
 
+// 각 센서 최소 최댓값 지정 
+const float TEMP_MIN = -50.0;
+const float TEMP_MAX = 100.0;
+const float HUM_MIN = 0.0;
+const float HUM_MAX = 100.0;
+const int PPM_MIN = 10;
+const int PPM_MAX = 10000;
+
 DHT dht(DHT11_PIN, DHT11_TYPE);
 MQ135 mq135(MQ135_PIN);
 
@@ -12,52 +20,44 @@ unsigned long nowSec = 0;
 unsigned long prevSec = 0;
 unsigned int interval_sec = 2;  // 측정 간격 (초)
 
-String Now;  // PC 시간 저장할 전역변수
 
-void timeUpdate() {
-  if (Serial.available()) {
-    Now = Serial.readStringUntil('\n');  // PC에서 시간 받아서 변수에 저장
-  }
-}
+
+
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
   prevSec = millis() / 1000;
-  Serial.println("시간 데이터 기다리는중 . . . ");
-  while (!Serial.available());
-  timeUpdate();
+
   Serial.println("");
+
   // header
-  Serial.print("현재 시간 : ");
-  Serial.println(Now);
-  Serial.println("온도\t습도\t공기질");
+  Serial.println("시간\t온도\t습도\t공기질");
 }
 
-
 void loop() {
-  timeUpdate();
   nowSec = millis() / 1000;
 
   if (nowSec - prevSec >= interval_sec) {
+    // 각 센서 데이터
     float temp = dht.readTemperature();
     float humi = dht.readHumidity();
     int ppm = mq135.getPPM();
 
-
-    // 오류 감지
-    if (isnan(temp) || isnan(humi))
-   {
-      Serial.println("dht11센서 인식 불가");
+    // 데이터 유효성 검사
+    if (isnan(temp) || isnan(humi)) {
+      Serial.println("ERROR_DHT11");
     }
-    // 현재 mq135센서 망가져서 인식 불가.
-    /*else if(ppm < 10 || ppm > 10000) 
-    {
-  Serial.println("mq135센서 인식 불가");
-    }*/ 
-  else
-    {
-      Serial.print(temp, 1);   // 소수점 1자리
+    else if (temp < TEMP_MIN || temp > TEMP_MAX || 
+             humi < HUM_MIN || humi > HUM_MAX) {
+      Serial.println("ERROR_RANGE_DHT11");
+    }
+    else if (ppm < PPM_MIN || ppm > PPM_MAX) {
+      Serial.println("ERROR_MQ135");
+    }
+    else {
+      // 시간 온도 습도 공기질 순으로 데이터 추출 
+      Serial.print(temp, 1);
       Serial.print("\t");
       Serial.print(humi, 1);
       Serial.print("\t");
